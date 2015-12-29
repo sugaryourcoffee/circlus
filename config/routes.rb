44a6de6d1463d177493d22af8aab71a99226ec41
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-12-29 23:55:02 +0100

    add routes for root, help and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  root              'static_pages#home'
 
-  get 'static_pages/about'
+  get  'help',  to: 'static_pages#help'
+
+  get  'about', to: 'static_pages#about'
 
   devise_for :users
+
   get "angular_test", to: "angular_test#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
