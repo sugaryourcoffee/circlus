278df03cd756d203fc857fbc30afdefa7ec40f48
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-12-29 14:33:04 +0100

    add basic content to home page

--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "static_pages/home.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should have the content 'Circlus'" do
+    render
+    expect(rendered).to have_text 'Circlus'
+  end
+
 end
