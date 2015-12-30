2932f015dc436602d0a964744f23d8af164c63d6
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-12-31 00:21:19 +0100

    implement index and new

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,19 +1,69 @@
 class OrganizationsController < ApplicationController
+
+  before_action :load_user
+
   def index
+    load_organizations
   end
 
   def new
+    build_organization
   end
 
   def create
+    build_organization
+    save_organization or render :new
   end
 
   def edit
+    load_organization
   end
 
   def update
+    build_organization
+    save_organization or render :edit
   end
 
   def destroy
+    load_organization
+    @organization.destroy
+    redirect_to organizations_path
   end
+
+  private
+
+    def load_user
+      @user ||= current_user
+    end
+
+    def load_organizations
+      @organizations = user_organizations
+    end
+
+    def load_organization
+      @organization ||= user_organizations.find(params[:id])
+    end
+
+    def build_organization
+      @organization ||= user_organizations.build
+      @organization.attributes = organization_params
+    end
+
+    def save_organization
+      if @organization.save
+        redirect_to organizations_path
+      end
+    end
+
+    def organization_params
+      organization_params = params[:organization]
+      organization_params ? organization_params
+                            .permit(:name, :street, :zip, :town,
+                                    :country, :email, :website,
+                                    :information) : {}
+    end
+
+    def user_organizations
+      @user.organizations
+    end
 end
