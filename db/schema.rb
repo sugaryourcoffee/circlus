9846ef6cc6b86404aa2c431702401fb23e6b35b3
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-12-31 00:17:35 +0100

    add schema for Organization

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228172735) do
+ActiveRecord::Schema.define(version: 20151230185141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "organizations", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.string   "street",      null: false
+    t.string   "zip",         null: false
+    t.string   "town",        null: false
+    t.string   "country",     null: false
+    t.string   "email"
+    t.string   "website"
+    t.text     "information"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
