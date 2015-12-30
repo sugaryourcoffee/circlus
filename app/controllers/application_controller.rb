9d50fb0d5de0c3796eb1249d2a8d67e3f9e00c4a
Pierre Sugar <pierre@sugaryourcoffee.de>
2015-12-31 00:20:03 +0100

    add redirect after login to organization index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Begin - SYC extenstions for Devise
+
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    organizations_path
+  end
+
+  # End - SYC extensions for Devise
+  
 end
