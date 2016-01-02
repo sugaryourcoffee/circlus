447a28a653ddbe8440a4ca758530aca1bf7de4c8
Pierre Sugar <pierre@sugaryourcoffee.de>
2016-01-03 00:03:41 +0100

    add input of event for Capybara test

---
+++ spec/support/event_support.rb
@@ -0,0 +1,15 @@
+visit groups_path
+click_link "View events..."
+click_link "Create new event"
+fill_in "Title", with: "Hackatron"
+fill_in "Description", with: "Hackatron event"
+fill_in "Cost", with: 1
+fill_in "Information", with: "BYOD"
+fill_in "Departure place", with: "Bus station"
+fill_in "Arrival place", with: "Train station"
+fill_in "Venue", with: "Sheraton"
+fill_in "Start date", with: "2016-01-02"
+fill_in "Start time", with: "6:00"
+fill_in "End date", with: "2016-01-03"
+fill_in "End time", with: "18:00"
+
