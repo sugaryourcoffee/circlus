7f5e7f46ba6da479f0532bfeacb184737a495ffe
Pierre Sugar <pierre@sugaryourcoffee.de>
2016-01-09 22:30:24 +0100

    create helpers for conditionally displaying phone, email, etc. if it is not nil or not empty

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,52 @@
 module ApplicationHelper
+
+  def if_content(content, br=true)
+    unless content.nil? or content.empty?
+      content << tag(:br) if br
+      content.html_safe
+    end
+  end
+
+  def if_phone(phone, br=true)
+    unless phone.nil? or phone.empty?
+      content = "<i class='glyphicon glyphicon-phone-alt'></i> #{phone}"
+      content << tag(:br) if br
+      content.html_safe
+    end
+  end
+
+  def if_email(email, br=true)
+    unless email.nil? or email.empty?
+      content = "<i class='glyphicon glyphicon-envelope'></i> #{mail_to email}"
+      content << tag(:br) if br
+      content.html_safe
+    end
+  end
+
+  def if_website(website, br=true, blank=true)
+    unless website.nil? or website.empty?
+      content = "<i class='glyphicon glyphicon-globe'></i> "
+      if blank
+        content << "#{link_to website, website, target: '_blank'}"
+      else
+        content << "#{link_to website, website}"
+      end
+      content << tag(:br) if br
+      content.html_safe
+    end
+  end
+
+  def if_blockquote(quote, footer)
+    unless quote.nil? or quote.empty?
+      content = <<-HTML
+        <blockquote><p>#{quote}</p>
+          <footer>Information about 
+            <cite>#{footer}</cite>
+          </footer>
+        </blockquote>
+      HTML
+      content.html_safe
+    end
+  end
+
 end
