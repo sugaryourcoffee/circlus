da83f53f71df4ea50e83f81b863bb38a4476400e
Pierre Sugar <pierre@sugaryourcoffee.de>
2016-07-18 07:36:15 +0200

    add orientation to title

--- app/helpers/pdf_templates_helper.rb
+++ app/helpers/pdf_templates_helper.rb
@@ -1,6 +1,6 @@
 module PdfTemplatesHelper
   def templates(klass)
     PdfTemplate.all.where('associated_class = ?', klass)
-                   .collect { |t| [t.title, t.id] }
+                   .collect { |t| ["#{t.title} (#{t.orientation})", t.id] }
   end
 end
